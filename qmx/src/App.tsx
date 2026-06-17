e8d4bc5ae0d36b459d43ebbcea4502842a299404
ah <me@hellings.cc>
2026-06-16 21:35:52 -0400

    Add fdx

--- qmx/src/App.tsx
+++ qmx/src/App.tsx
@@ -6,7 +6,6 @@ import { Banner } from './components/Banner';
 function App() {
   return (
     <>
-      <Banner />
       <img src={QMX} />
       <h1>A project by the <i>Functional Desktop Foundation</i>, qmX + bridge brings the best of unintrusive, UI-focused Qt Quick to all of your devices.</h1>
       <h2>It provides React-like states, IPC and shared storage, and remains easy to deploy across all platforms, given you have the proper hardware to do so.</h2>
@@ -15,6 +14,8 @@ function App() {
         color: 'gray',
         fontSize: '40px'
       }}>GitHub</a>
+
+      <iframe frameBorder={0} style={{ border: 0, outline: 0 }} src="https://donate.hellings.cc/" height="1080" width="750"></iframe>
     </>
   )
 }
